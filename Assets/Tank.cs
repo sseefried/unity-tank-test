b52946dbbd1abb49a2feec7090b5b2efbe123d8a
Sean Seefried <sean.seefried@gmail.com>
2020-02-22 14:47:45 +1000

    First commit

---
+++ Assets/Tank.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    [Range(0f,5f)]
+    [SerializeField] float movementSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector2(transform.position.x - movementSpeed * Time.deltaTime, transform.position.y); 
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Tank Test
+
+It took me a while to work out how to animate each part
+of the tank separately. What I didn't understand was
+that once you've created a hierarchy of sprite renderers
+under a GameObject then you can add each on of those
+sprites to the animator separately.
+
+I found this video which helped me "rediscover" this fact.
+
+https://www.youtube.com/watch?v=EmbA-AitPow
+
+It's from Noah from Black Thorn Productions. I watched some
+earlier videos of his which inspired me.
